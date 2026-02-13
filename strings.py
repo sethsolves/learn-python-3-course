8a00d047aa276e7dedc0843c26707f6675956872
Seth <142804088+sethsolves@users.noreply.github.com>
2026-02-13 15:00:47 +0330

    update

---
+++ strings.py
@@ -0,0 +1,4 @@
+#Computer programmers refer to blocks of text as
+#strings. A string is a sequence of characters
+#contained within a pair of single quotes or double quotes.
+print("Hello, I'm Seth, the vamp.")
